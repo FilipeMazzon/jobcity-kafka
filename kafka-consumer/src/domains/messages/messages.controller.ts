5f03c2fad7dba94a918e365a6de7a6d8a9156301
mazzon <filipe.mazzon.ribeiro@gmail.com>
2023-07-26 20:30:08 -0300

    add prefix message on controller in consumer

--- kafka-consumer/src/domains/messages/messages.controller.ts
+++ kafka-consumer/src/domains/messages/messages.controller.ts
@@ -4,7 +4,7 @@ import {MessagesService} from './messages.service';
 import {CreateMessageDto} from './dto/create-message.dto';
 import {MessageDocument} from "./message.schema";
 
-@Controller()
+@Controller('/message')
 export class MessagesController {
   constructor(private readonly messagesService: MessagesService) {}
 
